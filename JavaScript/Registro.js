bcf3584f71e470abd3c91f06b9d9742177b9abf0
Renann77 <renan.dorneles.potencial@gmail.com>
2024-06-05 11:46:07 -0300

    registro att

--- JavaScript/Registro.js
+++ JavaScript/Registro.js
@@ -1,22 +1,33 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+  const registerForm = document.getElementById("registerForm");
 
-  const registerUsername = document.getElementById('registerUsername').value;
-  const registerEmail = document.getElementById('registerEmail').value;
-  const registerPhone = document.getElementById('registerPhone').value;
-  const registerPassword = document.getElementById('registerPassword').value;
+  // Adiciona um ouvinte de evento para o formulário de registro
+  registerForm.addEventListener("submit", function(event) {
+      event.preventDefault(); // Evita o envio do formulário
 
-  // Armazenar as informações de registro em localStorage
-  localStorage.setItem('registerUsername', registerUsername);
-  localStorage.setItem('registerEmail', registerEmail);
-  localStorage.setItem('registerPhone', registerPhone);
-  localStorage.setItem('registerPassword', registerPassword);
+      // Obtém os valores do formulário
+      const username = document.getElementById("registerUsername").value;
+      const email = document.getElementById("registerEmail").value;
+      const phone = document.getElementById("registerPhone").value;
+      const password = document.getElementById("registerPassword").value;
 
-  alert('Registro bem-sucedido!');
+      // Cria um objeto com os dados do usuário
+      const userData = {
+          username: username,
+          email: email,
+          phone: phone,
+          password: password
+      };
 
-    window.location.href = 'Login.html';
-  // Limpar campos de registro
-  document.getElementById('registerForm').reset();
+      // Converte o objeto em uma string JSON
+      const userDataJSON = JSON.stringify(userData);
 
+      // Armazena os dados no localStorage
+      localStorage.setItem("userData", userDataJSON);
 
+      // Redireciona para a página de login ou faz outra ação, conforme necessário
+       window.location.href = "Login.html";
+      // ou
+      // Alguma outra ação após o registro
+  });
 });
